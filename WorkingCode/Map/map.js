04771f1be945c422b2536d9cfdc128d379f546e0
HasanAl-Quaid <hasan.alquaid@gmail.com>
2023-03-22 22:28:25 -0400

    Users Added to Map, 85% Done

--- WorkingCode/Map/map.js
+++ WorkingCode/Map/map.js
@@ -1,3 +1,7 @@
+let userIds = [];
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const firebaseConfig = {
     apiKey: "AIzaSyBxoV0Qji0j8GjG2N4jqYWWUtmUnN1Qyec",
@@ -24,6 +28,8 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
   
+    ID_Array();
+  
 });
 
 mapboxgl.accessToken = 'pk.eyJ1IjoicHJpbmNla2hyb25vcyIsImEiOiJjbGR5M2FjZXcwMWZwM25vOGRzZ2NyaGNtIn0.xuJooVDUksV_s8W29Jwu7w';
@@ -39,11 +45,63 @@ function initMap() {
   });
 }
 
+
+
+
+async function ID_Array() {
+  const db = firebase.firestore();
+
+  const createAccountSnapshot = await db.collection("CreateAccount").get();
+
+
+  if (!createAccountSnapshot.empty) {
+    createAccountSnapshot.forEach((doc) => {
+      const data = doc.data();
+      const userID = data.ID;
+
+      if (userID !== undefined) {
+
+        userIds.push(userID);
+      }
+    });
+
+  } else {
+    console.log("No documents found in the CreateAccount collection.");
+  }
+
+
+}
+
+window.onload
+
 async function displayAddresses() {
   clearMarkers();
-  // await geocodeAddress("777 W Harrisburg Pike, Middletown, PA 17057","Sukmoon Blum");
-  // await geocodeAddress("277 W Main St, Middletown, PA 17057","Jeremy Chang");
+  
+
+  const db = firebase.firestore();
+
+  console.log(userIds.length);
+  for(let i =0; i <userIds.length;i++){
+    //console.log(userIds[i])
+    const preferences = await db.collection(userIds[i]).doc('Preferences').get();
+
+  if (preferences.exists) {
+    const username = preferences.data().username;
+    const address = preferences.data().address;
+    console.log("Username: ",username)
+    console.log("address: ",address)
+  
+    await geocodeAddress(address, username);
+  } else {
+    console.log('Preferences document does not exist');
+  }
+  }
+  
+ 
 }
+
+
+
 async function geocodeAddress(address,name) {
     const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${mapboxgl.accessToken}`);
     const data = await response.json();
